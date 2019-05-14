aa77715e6db47958f99491b0ec6e0a09a53d27a5
Daniele <d.rizzo0@icloud.com>
2019-05-14 14:33:21 +0200

    1.0.4 Fixed

--- cleanlog.bat
+++ cleanlog.bat
@@ -1,11 +1,11 @@
 @echo off
 
-set "log_path=C:\Users\%USERNAME%\log"
+set "log_path=C:\Users\%USERNAME%\log\"
 
 echo Starting log cleaner.
 echo Check log folder
 
-IF NOT EXIST %log_path% GOTO log_not_exists ELSE GOTO log_exists
+IF EXIST %log_path% GOTO log_exists ELSE GOTO log_not_exists
 
 :log_not_exists
 echo Log folder does not exists.
@@ -16,7 +16,7 @@ exit
 echo Log folder exists.
 rmdir /S /Q %log_path%
 echo Checking if log folder were deleted.
-IF EXISTS %log_path% GOTO log_exists
+IF EXIST %log_path% GOTO log_exists
 echo Log folder deleted.
 echo All done.
 pause
