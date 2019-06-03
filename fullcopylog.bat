6b698db61a0e71686d283c2aa8a00bf2445570b3
drizzo0 <48352006+drizzo0@users.noreply.github.com>
2019-06-03 10:13:05 +0200

    Update fullcopylog.bat
    added variable for password

--- fullcopylog.bat
+++ fullcopylog.bat
@@ -1,21 +1,22 @@
 @echo off
 
+rem SETTING VARIABLES
+
+set "netadd=192.168.168.xx"
+set "password=passwoed"
+
+set "net_path=Q:\\%netadd%\aaaaaaa /user:%USERNAME% %password%"
+set "source_path=c:\Users\%USERNAME%\Documents\File di Outlook"
+set "dest_path=Q:\bksett1\posta"
+
 rem SETTING DATE
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 
-rem SETTING VARIABLES
-
-set "netadd=192.168.168.xx"
-
-set "net_path=Q:\\%netadd%\aaaaaaa /user:%USERNAME%"
-
 set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
 set "log_path=C:\Users\%USERNAME%\log"
 set "full_log_path=%log_path%\%fullstamp%_full.log"
-set "source_path=c:\Users\%USERNAME%\Documents\File di Outlook"
-set "dest_path=Q:\bksett1\posta"
 
 IF NOT EXIST %log_path% mkdir %log_path%
 
